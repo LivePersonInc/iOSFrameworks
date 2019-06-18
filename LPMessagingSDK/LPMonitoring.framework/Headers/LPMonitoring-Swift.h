aace739468ac780338fc439857091173b9b8c40f
ci_user <ci_user@liveperson.com>
2019-06-19 00:18:06 +0300

    Update version 3.9.1.0

--- LPMessagingSDK/LPMonitoring.framework/Headers/LPMonitoring-Swift.h
+++
@@ -1,6 +0,0 @@
- 
-#if TARGET_OS_SIMULATOR
-  #import "LPMonitoring-Simulator-Swift.h"
-#else
-  #import "LPMonitoring-Device-Swift.h"
-#endif
---
+++ LPMessagingSDK/LPMonitoringSDK.framework/Headers/LPMonitoringSDK.h
@@ -0,0 +1,19 @@
+//
+//  LPMonitoringSDK.h
+//  LPMonitoringSDK
+//
+//  Created by Nir Lachman on 12/11/2017.
+//  Copyright © 2017 Liveperson Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LPMonitoringSDK.
+FOUNDATION_EXPORT double LPMonitoringSDKVersionNumber;
+
+//! Project version string for LPMonitoringSDK.
+FOUNDATION_EXPORT const unsigned char LPMonitoringSDKVersionString[];
+
+// In this header, you should import all the  headers of your framework using statements like #import <LPMonitoringSDK/Header.h>
+
+
